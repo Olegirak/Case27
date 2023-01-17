2b0b3a26b89925b49a7e58bd3c8abf0e3409b2a1
Oleg Iraklionov <irakliov@mail.ru>
2023-01-17 20:27:06 +0300

    Case 27 is ready

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿ //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+Console.Write("Введите число N ");
+int N = int.Parse(Console.ReadLine()!);
+
+int SumOfDigits(int N)
+{
+    int result = 0;
+    while( N/10 > 0)
+    {
+        result  = result+N%10;
+        N = N/10;
+    }
+    return result+N;
+}
+Console.WriteLine(SumOfDigits(N));
